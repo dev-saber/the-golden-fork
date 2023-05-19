f66d16ee292748866f8d3b1e35edd5e013201a20
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-19 16:00:43 +0100

    meals migration

---
+++ server/database/migrations/2023_05_19_145514_create_meals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained()->cascadeOnDelete();
+            $table->string('meal_name');
+            $table->string('image_path');
+            $table->float('meal_price', 4);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meals');
+    }
+};
