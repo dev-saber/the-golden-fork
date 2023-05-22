2eb65b8e6f83f145b6aa48a81e212c5bfc6d47be
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-22 13:21:29 +0100

    customer / revservation / order model, migration && relationships

---
+++ server/database/migrations/2023_05_22_120646_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('client_address');
+            $table->dateTime('delivery_time');
+            $table->float('amount', 4, 2);
+            $table->enum('payment_option', ['PayPal', 'Stripe']);
+            $table->foreignId('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
