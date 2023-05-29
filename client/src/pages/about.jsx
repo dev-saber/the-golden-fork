11b44c619e225a9bb8d91e9fa8ab0451b69336e7
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-29 22:41:14 +0100

    about section responsiveness

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -5,15 +5,15 @@ import Dish from "../assets/images/meal-dish-img.png";
 
 export default function About() {
   return (
-    <div className="mt-8 flex flex-col items-center gap-8 md:mt-16 md:gap-24">
+    <div className="mt-8 flex flex-col items-center gap-8 md:gap-16 md:mt-16 lg:gap-24">
       <Title text="About" />
-      <div className="flex flex-col items-center gap-8 md:flex-row md:gap-0 md:px-32">
+      <div className="flex flex-col items-center gap-8 lg:flex-row lg:gap-0 lg:px-32">
         <img
           src={Restaurant}
           alt="restaurant-img"
           className="w-[70%] md:w-[390px]"
         />
-        <p className="font-poppins text-white px-16 text-justify md:text-2xl">
+        <p className="font-poppins text-white px-16 text-justify md:text-xl md:w-[85%] lg:text-2xl">
           The Golden Fork is a sophisticated high-end restaurant with an elegant
           decor and a carefully crafted menu. The restaurant's focus is on using
           only the freshest, highest-quality ingredients, with an emphasis on
@@ -22,9 +22,9 @@ export default function About() {
           occasion.
         </p>
       </div>
-      <div className="flex flex-col items-center gap-8 md:gap-0 md:flex-row-reverse md:justify-around">
+      <div className="flex flex-col items-center gap-8 md:gap-12 lg:gap-0 lg:flex-row-reverse lg:justify-around">
         <img src={Dish} alt="meal-dish-img" className="w-[70%] md:w-[390px]" />
-        <p className="glow font-bold text-xl px-8 text-center font-yellowtail md:text-5xl md:w-[50%]">
+        <p className="glow font-bold text-xl px-8 text-center font-yellowtail md:text-4xl md:w-[70%] lg:text-5xl lg:w-[50%]">
           "Every dish is a celebration of fresh and high-quality ingredients."
         </p>
       </div>
