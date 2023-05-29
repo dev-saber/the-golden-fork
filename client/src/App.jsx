9e9bb84e0741e90dce28a842039f0f5fb9e2b0ee
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-29 23:26:58 +0100

    layouts folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,9 @@
-import About from "./pages/about";
 import Home from "./pages/home";
-import Menu from "./pages/menu";
-import Purchase from "./pages/purchase";
 
 function App() {
   return (
     <>
       <Home />
-      <div className="bg-bgBlack gap-24 flex flex-col md:gap-32">
-        <About />
-        <Menu/>
-        <Purchase/>
-      </div>
     </>
   );
 }
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,20 +1,19 @@
-import React from "react";
-import NavBar from "../components/navBar";
-import LandingImage from "../assets/images/landing-img.png";
+import React from 'react';
+import Landing from '../layouts/landing';
+import About from '../layouts/about';
+import Menu from '../layouts/menu';
+import Purchase from '../layouts/purchase';
+
 
 export default function Home() {
   return (
-    <div
-      className="relative bg-center bg-cover bg-no-repeat bg-image-url w-full h-[100vh] font-yellowtail"
-      style={{ backgroundImage: `url(${LandingImage})` }}
-    >
-      <NavBar />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-4/5 flex flex-col items-center md:gap-2">
-        <p className="glow text-5xl md:text-7xl">The Golden Fork</p>
-        <p className="text-white text-xl md:text-3xl">
-          Elevating the Art of Dining
-        </p>
+    <>
+      <Landing />
+      <div className="bg-bgBlack gap-24 flex flex-col md:gap-32">
+        <About />
+        <Menu/>
+        <Purchase/>
       </div>
-    </div>
+    </>
   );
 }
