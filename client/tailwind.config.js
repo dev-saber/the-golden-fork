03d51ef94db8d6b2150a74a2b294ebc631fd401c
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-29 13:14:47 +0100

    tailwind brand colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'goldenYellow' : '#ED9100',
+        'bgBlack' : '#0F0A06',
+      }
+    },
   },
   plugins: [],
 }
