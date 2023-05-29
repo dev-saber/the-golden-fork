5bb6f5bf50366035da2c11a5ecb9940483526e10
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-29 13:34:42 +0100

    react flowbite installation

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,14 +3,16 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
+  plugins: [require("flowbite/plugin")],
   theme: {
     extend: {
       colors: {
-        'goldenYellow' : '#ED9100',
-        'bgBlack' : '#0F0A06',
-      }
+        goldenYellow: "#ED9100",
+        bgBlack: "#0F0A06",
+      },
     },
   },
   plugins: [],
-}
+};
