766dae58c11af8dba6b98e0e3b2b926e22da016b
sifeddineEddr <sifeddine.edr@gmail.com>
2023-05-30 17:57:01 +0100

    menu page layout

---
+++ client/src/components/meal.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+export default function Meal({ title, price }) {
+  const [imageSrc, setImageSrc] = useState(null);
+
+  useEffect(() => {
+    import(`../assets/images/${title}-img.png`)
+      .then((image) => {
+        setImageSrc(image.default);
+      })
+      .catch((error) => {
+        console.error(`Failed to load image: ${error}`);
+      });
+  }, [title]);
+
+  return (
+    <div className="flex items-center w-4/5 sm:w-auto">
+      <div className="max-w-[20%] mx-5">
+        <img src={imageSrc} alt="" className="w-full h-auto" />
+      </div>
+      <div className="flex flex-col items-start">
+        <p className="font-yellowtail text-lg sm:text-xl text-white">{title}</p>
+        <p className="font-poppins text-sm sm:text-base">{price} MAD</p>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/menu.jsx
+++ client/src/pages/menu.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import Title from "../components/title";
-import MenuCategory from "../components/menuCategory";
 import MenuData from "../services/menuData";
+import Meal from "../components/meal";
+import Footer from "../components/footer";
 
 export default function Menu() {
   const data = MenuData();
-  const content = data.map(category => {
-    console.log(category.meals)
-   return (
-    <div className="flex flex-col items-center">
+  const content = data.map((category) => {
+    return (
+      <div className="flex flex-col items-center">
         <Title text={category.title} />
-        <div className="flex flex-col items-center gap-12 lg:flex-row lg:gap-56">
-        {category.meals.slice(0, 2).map((meal) => {
-          return 
-        })}
+
+        <div className="flex flex-col items-start gap-8">
+          {category.meals.map((meal) => {
+            return <Meal title={meal.meal_name} price={meal.meal_price} />;
+          })}
+        </div>
       </div>
-      <div className="flex flex-col items-center gap-12 lg:flex-row lg:gap-56">
-        
-        {category.meals.slice(2).map((meal) => {
-          return 
-        })}
-      </div>
-    </div>
-   )
-  })
+    );
+  });
   return (
-    <div className="bg-bgBlack w-full h-[100%] gap-24 flex flex-col md:gap-32 text-white">
+    <div className="bg-bgBlack w-full h-[100%] pt-20 gap-24 flex flex-col md:gap-32 text-white">
       {content}
+      <Footer />
     </div>
   );
 }
