85b4ba952b6b30c9c963fa1e9fdb4864e36978d1
sifeddineEddr <sifeddine.edr@gmail.com>
2023-06-01 11:24:02 +0100

    links animations

--- client/src/components/backArrow.jsx
+++ client/src/components/backArrow.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 
 export default function BackArrow() {
   const navigate = useNavigate();
-  const handleNavigation = () => {};
   return (
-    <div className="absolute left-24 lg:left-28 top-24">
-        <Link to="/"><img
-    src="/images/arrow.svg"
-    className="w-[32px] cursor-pointer"
-    alt="arrow"
-  /></Link>
+    <div
+      className="absolute left-24 lg:left-28 top-24"
+      onClick={() => navigate("/")}
+    >
+      <motion.img
+        src="/images/arrow.svg"
+        className="w-[32px] cursor-pointer"
+        alt="arrow"
+        whileTap={{
+          scale: 0.8,
+          x: -50,
+        }}
+      />
     </div>
-    
-    
   );
 }
--- client/src/components/navLink.jsx
+++ client/src/components/navLink.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 export default function NavLink({ text, navigation }) {
   const navigate = useNavigate();
   return (
-    <p
+    <motion.p
       className="font-poppins text-goldenYellow font-bold cursor-pointer hover:underline md:text-xl lg:ml-16"
       onClick={() => navigate(`/${navigation}`)}
+      whileTap={{
+        scale: 0.8,
+      }}
     >
       {text}
-    </p>
+    </motion.p>
   );
 }
