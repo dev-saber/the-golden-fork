afc87f57cac99afb9bdadb9bade97d213aceeed5
sifeddineEddr <sifeddine.edr@gmail.com>
2023-06-08 14:54:39 +0100

    stripe first set up

---
+++ client/src/layouts/order/paymentForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+function paymentForm() {
+  return <div>paymentForm</div>;
+}
+
+export default paymentForm;
