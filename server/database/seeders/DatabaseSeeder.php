673e8ac6418c5778791e8d8071499320bfaa4341
sifeddineEddr <sifeddine.edr@gmail.com>
2023-06-09 16:17:35 +0100

    stripe product id migration

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -35,25 +35,30 @@ class DatabaseSeeder extends Seeder
                         'meal_name' => 'Golden Fried Calamari with Spicy Aioli',
                         'meal_price' => 28,
                         'menu_id' => $menu->id,
-                        'image_path' => 'Golden Fried Calamari with Spicy Aioli-img.png'
+                        'image_path' => 'Golden Fried Calamari with Spicy Aioli-img.png',
+                        'stripe_id' => 'prod_O2pmlfs6262Ef3'
+
                     ]);
                     Meal::create([
                         'meal_name' => 'Roasted Red Pepper Hummus with Pita Chips',
                         'meal_price' => 18,
                         'menu_id' => $menu->id,
                         'image_path' => 'Roasted Red Pepper Hummus with Pita Chips-img.png',
+                        'stripe_id' => 'prod_O38bYVOGUqMReb'
                     ]);
                     Meal::create([
                         'meal_name' => 'Crab Cakes with Lemon-Caper Aioli',
                         'meal_price' => 35,
                         'menu_id' => $menu->id,
                         'image_path' => 'Crab Cakes with Lemon-Caper Aioli-img.png',
+                        'stripe_id' => 'prod_O38slYEBsn2WVN'
                     ]);
                     Meal::create([
                         'meal_name' => 'Spinach and Artichoke Dip with Toasted Baguette',
                         'meal_price' => 22,
                         'menu_id' => $menu->id,
                         'image_path' => 'Spinach and Artichoke Dip with Toasted Baguette-img.png',
+                        'stripe_id' => 'prod_O38vWKXuPDOkrT'
                     ]);
                     break;
 
@@ -63,24 +68,28 @@ class DatabaseSeeder extends Seeder
                         'meal_price' => 30,
                         'menu_id' => $menu->id,
                         'image_path' => 'Classic Caesar Salad with Grilled Chicken or Shrimp-img.png',
+                        'stripe_id' => 'prod_O38wDQzWLC05fa'
                     ]);
                     Meal::create([
                         'meal_name' => 'Caprese Salad with Fresh Mozzarella and Tomatoes',
                         'meal_price' => 26,
                         'menu_id' => $menu->id,
                         'image_path' => 'Caprese Salad with Fresh Mozzarella and Tomatoes-img.png',
+                        'stripe_id' => 'prod_O38xfGf4e5lXA4'
                     ]);
                     Meal::create([
                         'meal_name' => 'Arugula and Pear Salad with Candied Walnuts and Cheese',
                         'meal_price' => 28,
                         'menu_id' => $menu->id,
                         'image_path' => 'Arugula and Pear Salad with Candied Walnuts and Cheese-img.png',
+                        'stripe_id' => 'prod_O38yP3VMLf5mra'
                     ]);
                     Meal::create([
                         'meal_name' => 'Asian Chicken Salad with Sesame Ginger Dressing',
                         'meal_price' => 30,
                         'menu_id' => $menu->id,
                         'image_path' => 'Asian Chicken Salad with Sesame Ginger Dressing-img.png',
+                        'stripe_id' => 'prod_O38zAQd8FR1sCC'
                     ]);
                     break;
 
@@ -90,24 +99,28 @@ class DatabaseSeeder extends Seeder
                         'meal_price' => 40,
                         'menu_id' => $menu->id,
                         'image_path' => 'Linguine with Clams and White Sauce-img.png',
+                        'stripe_id' => 'prod_O3901oJLog3LBA'
                     ]);
                     Meal::create([
                         'meal_name' => 'Fettuccine Alfredo with Grilled Chicken or Shrimp',
                         'meal_price' => 35,
                         'menu_id' => $menu->id,
                         'image_path' => 'Fettuccine Alfredo with Grilled Chicken or Shrimp-img.png',
+                        'stripe_id' => 'prod_O390z1uMUYzbDx'
                     ]);
                     Meal::create([
                         'meal_name' => 'Spaghetti Carbonara with Pancetta and Peas',
                         'meal_price' => 35,
                         'menu_id' => $menu->id,
                         'image_path' => 'Spaghetti Carbonara with Pancetta and Peas-img.png',
+                        'stripe_id' => 'prod_O392jzJh3WGbcb'
                     ]);
                     Meal::create([
                         'meal_name' => 'Penne with Spicy Sausage and Tomato Sauce',
                         'meal_price' => 30,
                         'menu_id' => $menu->id,
                         'image_path' => 'Penne with Spicy Sausage and Tomato Sauce-img.png',
+                        'stripe_id' => 'prod_O394rfH9ce2jWj'
                     ]);
                     break;
 
@@ -117,24 +130,28 @@ class DatabaseSeeder extends Seeder
                         'meal_price' => 20,
                         'menu_id' => $menu->id,
                         'image_path' => 'Chocolate Lava Cake with Vanilla Ice Cream-img.png',
+                        'stripe_id' => 'prod_O398oAK6bJ0PbU'
                     ]);
                     Meal::create([
                         'meal_name' => 'Tiramisu with Espresso and Whipped Cream',
                         'meal_price' => 22,
                         'menu_id' => $menu->id,
                         'image_path' => 'Tiramisu with Espresso and Whipped Cream-img.png',
+                        'stripe_id' => 'prod_O39CGIGBuNKCbI'
                     ]);
                     Meal::create([
                         'meal_name' => 'Fruit Tart with Whipped Cream',
                         'meal_price' => 25,
                         'menu_id' => $menu->id,
                         'image_path' => 'Fruit Tart with Whipped Cream-img.png',
+                        'stripe_id' => 'prod_O39E0eCLW67TSg'
                     ]);
                     Meal::create([
                         'meal_name' => 'Apple Tart with Cinnamon Ice Cream',
                         'meal_price' => 20,
                         'menu_id' => $menu->id,
                         'image_path' => 'Apple Tart with Cinnamon Ice Cream-img.png',
+                        'stripe_id' => 'prod_O39FUDyxPhGL3K'
                     ]);
                     break;
 
